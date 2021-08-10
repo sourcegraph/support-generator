b50be9d4908fc742f5423fe8ca60da932888b424
Jason Harris <sourcegraph@Jasons-MacBook-Pro.local>
2021-08-10 11:17:52 -0700

    adds base functionality for picking deployment type.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// import {useState} from 'react'
 import './App.css';
+import Deployment from './Components/Deployment/Deployment';
+import Action from './Components/Action/Action';
+import Command from './Components/Command/Command';
 
 function App() {
+  // add deployment type state here so it can be passed around to different components.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Command Line Generator</h1>
+      <div className="actions">
+        <Deployment />
+        <Action />
+      </div>
+      <div>
+        <Command />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// deployment component
+// function component (will change based on deployment component)
+// command component (copyable)
