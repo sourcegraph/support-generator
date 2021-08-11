e35fc1fd8423b7c3ba4852aedcc0220dc7cca76f
Jason Hawk Harris <jasonhawkharris@gmail.com>
2021-08-11 13:27:31 -0700

    clean up.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
 import {useState} from 'react'
-import './App.css';
 import Deployment from './Components/Deployment/Deployment';
 import Action from './Components/Action/Action';
 import Command from './Components/Command/Command';
 
-function App() {
-  // add deployment type state here so it can be passed around to different components.
-  const [selectedDeployment, setSelectedDeployment] = useState("Select Deployment");
-  const [selectedAction, setSelectedAction] = useState("Function")
-  const [hasNamespace, setHasNamespace] = useState(false);
-  const [namespace, setNamespace] = useState("");
+import './App.css';
 
-  return (
-    <div className="App">
-      <h1>Command Line Generator</h1>
-      <div className="actions">
-        <Deployment
-          selectedDeployment={selectedDeployment}
-          setSelectedDeployment={setSelectedDeployment}
-          hasNamespace={hasNamespace}
-          setHasNamespace={setHasNamespace}
-          namespace={namespace}
-          setNamespace={setNamespace}
-        />
-        <Action
-          selectedDeployment={selectedDeployment}
-          selectedAction={selectedAction}
-          setSelectedAction={setSelectedAction}
-        />
-      </div>
-      <div>
-        <Command />
-      </div>
-    </div>
-  );
+function App() {
+	const [selectedDeployment, setSelectedDeployment] = useState("Select Deployment");
+	const [selectedAction, setSelectedAction] = useState("Function")
+	const [hasNamespace, setHasNamespace] = useState(false);
+	const [namespace, setNamespace] = useState("");
+
+	return (
+		<div className="App">
+			<h1>Command Line Generator</h1>
+			<div className="actions">
+				<Deployment
+					selectedDeployment={selectedDeployment}
+					setSelectedDeployment={setSelectedDeployment}
+					hasNamespace={hasNamespace}
+					setHasNamespace={setHasNamespace}
+					namespace={namespace}
+					setNamespace={setNamespace}
+				/>
+				<Action
+					selectedDeployment={selectedDeployment}
+					selectedAction={selectedAction}
+					setSelectedAction={setSelectedAction}
+				/>
+			</div>
+			<div>
+				<Command />
+			</div>
+		</div>
+	);
 }
 
 export default App;
-
-// deployment component
-// function component (will change based on deployment component)
-// command component (copyable)
